caa1d618468b1e5e2548b22bb3696c759454f622
Javier Manobanda <51manobandajavier@gmail.com>
2023-10-17 16:34:11 +0200

    update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b -p conda
-conda/bin/conda create --name framework python=3.10
+conda/bin/conda create --name cvenv --file ./env.yml
 
-export PATH=~/conda/envs/framework/bin:~/conda/bin:$PATH
-source activate framework
+export PATH=~/conda/envs/cvenv/bin:~/conda/bin:$PATH
+source activate cvenv
