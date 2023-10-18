5487941b554aee1234bd2fab52bf2b35de492a8c
LUIS JAVIER MANOBANDA TUTASIG <v839330@login2.hpc.lan>
2023-10-18 13:03:24 +0000

    add new configuration

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##----------------------- Start job description -----------------------
 #SBATCH --job-name=example
-#SBATCH --time=07:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=standard-gpu
